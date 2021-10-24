5092dd9fa32202af72e3070ff01d716bccd6e344
Liz Ireland <liz.t.ireland@gmail.com>
2021-10-23 21:35:53 -0700

    updating functions in script.js

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ var timeDisplayEl = $('#time-display');
 var projectDisplayEl = $('#project-display');
 // var projectModalEl = $('#project-modal');
 var projectFormEl = $('#project-form');
-// var projectNameInputEl = $('#project-name-input');
-// var projectTypeInputEl = $('#project-type-input');
-// var hourlyRateInputEl = $('#hourly-rate-input');
-// var dueDateInputEl = $('#due-date-input');
 var hourDisplayEl = $('#hour-display');
 
 // handle displaying the time
@@ -27,53 +23,82 @@ function displayHour() {
   hourDisplayEl.text(moment({hour: 9}));
 }
 
-
-function printProjectData(name, type, hourlyRate, dueDate) {
-  var projectRowEl = $('<tr>');
-
-  var projectNameTdEl = $('<td>').addClass('p-2').text(name);
-
-  var projectTypeTdEl = $('<td>').addClass('p-2').text(type);
-
-  var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
-
-  var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
-
-  var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
-  var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
-
-  var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
-
-  // You can also chain methods onto new lines to keep code clean
-  var totalTdEl = $('<td>')
-    .addClass('p-2')
-    .text('$' + totalEarnings);
-
-  var deleteProjectBtn = $('<td>')
-    .addClass('p-2 delete-project-btn text-center')
-    .text('X');
-
-  // By listing each `<td>` variable as an argument, each one will be appended in that order
-  projectRowEl.append(
-    projectNameTdEl,
-    projectTypeTdEl,
-    rateTdEl,
-    dueDateTdEl,
-    daysLeftTdEl,
-    totalTdEl,
-    deleteProjectBtn
-  );
-
-  projectDisplayEl.append(projectRowEl);
-
-  projectModalEl.modal('hide');
+function changeColor () {
+  $(".timeBlock").each(function (){
+   var time = $(this).data("time")
+   var current = 13//moment().hours()
+  console.log(time)
+  console.log(current)
+  if (time < current) {
+    $(this).css("background-color","gray")
+  } else if (time > current) {
+    $(this).css("background-color","green")
+  } else if (time === current) {
+    $(this).css("background-color","red")
+  }
+  })
 }
 
-function calculateTotalEarnings(rate, days) {
-  var dailyTotal = rate * 8;
-  var total = dailyTotal * days;
-  return total;
+changeColor()
+
+function saveData (event) {
+ var text = $(this).parent().parent().siblings(".td-class").children(".input-group").children("input").val()
+ console.log(text)
 }
+$(".btn").each(function (){
+  $(this).on("click", saveData)
+})
+
+function printData () {
+  var text = $(this).parent().parent().siblings(".td-class").children(".input-group").children("input").val()
+  var projectRowEl = $('<p>').addClass('project-display');
+  projectRowEl.append(text);
+ }
+ $(".btn").each(function (){
+   $(this).on("click", printData)
+ })
+
+
+// function printProjectData(name, type, hourlyRate, dueDate) {
+//   var projectRowEl = $('<tr>');
+
+//   var projectNameTdEl = $('<td>').addClass('p-2').text(name);
+
+//   var projectTypeTdEl = $('<td>').addClass('p-2').text(type);
+
+//   var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
+
+//   var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
+
+//   var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
+//   var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
+
+//   var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
+
+//   // You can also chain methods onto new lines to keep code clean
+//   var totalTdEl = $('<td>')
+//     .addClass('p-2')
+//     .text('$' + totalEarnings);
+
+//   var deleteProjectBtn = $('<td>')
+//     .addClass('p-2 delete-project-btn text-center')
+//     .text('X');
+
+//   // By listing each `<td>` variable as an argument, each one will be appended in that order
+//   projectRowEl.append(
+//     projectNameTdEl,
+//     projectTypeTdEl,
+//     rateTdEl,
+//     dueDateTdEl,
+//     daysLeftTdEl,
+//     totalTdEl,
+//     deleteProjectBtn
+//   );
+
+//   projectDisplayEl.append(projectRowEl);
+
+//   projectModalEl.modal('hide');
+// }
 
 function handleDeleteProject(event) {
   console.log(event.target);
