6bde8bf1b4ff337a88d271fc934594be24c46a50
Liz Ireland <liz.t.ireland@gmail.com>
2021-10-26 19:18:44 -0700

    final edits

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ function displayHour() {
 function changeColor () {
   $(".timeBlock").each(function (){
    var time = $(this).data("time")
-   var current = 13//moment().hours()
+   $(this).children(".td-class").children(".input-group").children(".form-control").val(localStorage.getItem(time))
+   var current = moment().hours()
   console.log(time)
   console.log(current)
   if (time < current) {
@@ -41,88 +42,27 @@ function changeColor () {
 
 changeColor()
 
-function saveData (event) {
+function saveData (text) {
  var text = $(this).parent().parent().siblings(".td-class").children(".input-group").children("input").val()
+ var time = $(this).parent().parent().parent().data("time")
  console.log(text)
+ localStorage.setItem(time, text)
 }
 $(".btn").each(function (){
   $(this).on("click", saveData)
 })
 
-function printData () {
-  var text = $(this).parent().parent().siblings(".td-class").children(".input-group").children("input").val()
-  var projectRowEl = $('<p>').addClass('project-display');
-  projectRowEl.append(text);
+function printData (text) {
+  var projectDisplayEl = $('<p>').addClass('project-display').text(task);
+
+  projectDisplayEl.append(text)
+  console.log("hi")
+  
  }
- $(".btn").each(function (){
-   $(this).on("click", printData)
- })
+ 
 
 
-// function printProjectData(name, type, hourlyRate, dueDate) {
-//   var projectRowEl = $('<tr>');
 
-//   var projectNameTdEl = $('<td>').addClass('p-2').text(name);
-
-//   var projectTypeTdEl = $('<td>').addClass('p-2').text(type);
-
-//   var rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
-
-//   var dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
-
-//   var daysToDate = moment(dueDate, 'MM/DD/YYYY').diff(moment(), 'days');
-//   var daysLeftTdEl = $('<td>').addClass('p-2').text(daysToDate);
-
-//   var totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
-
-//   // You can also chain methods onto new lines to keep code clean
-//   var totalTdEl = $('<td>')
-//     .addClass('p-2')
-//     .text('$' + totalEarnings);
-
-//   var deleteProjectBtn = $('<td>')
-//     .addClass('p-2 delete-project-btn text-center')
-//     .text('X');
-
-//   // By listing each `<td>` variable as an argument, each one will be appended in that order
-//   projectRowEl.append(
-//     projectNameTdEl,
-//     projectTypeTdEl,
-//     rateTdEl,
-//     dueDateTdEl,
-//     daysLeftTdEl,
-//     totalTdEl,
-//     deleteProjectBtn
-//   );
-
-//   projectDisplayEl.append(projectRowEl);
-
-//   projectModalEl.modal('hide');
-// }
-
-function handleDeleteProject(event) {
-  console.log(event.target);
-  var btnClicked = $(event.target);
-  btnClicked.parent('tr').remove();
-}
-
-// handle project form submission
-function handleProjectFormSubmit(event) {
-  event.preventDefault();
-
-  var projectName = projectNameInputEl.val().trim();
-  var projectType = projectTypeInputEl.val().trim();
-  var hourlyRate = hourlyRateInputEl.val().trim();
-  var dueDate = dueDateInputEl.val().trim();
-
-  printProjectData(projectName, projectType, hourlyRate, dueDate);
-
-  projectFormEl[0].reset();
-}
-
-projectFormEl.on('submit', handleProjectFormSubmit);
-projectDisplayEl.on('click', '.delete-project-btn', handleDeleteProject);
-// dueDateInputEl.datepicker({ minDate: 1 });
 
 setInterval(displayTime, 1000);
 setInterval(displayHour, 1000);
